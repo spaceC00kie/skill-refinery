688fcf50be80d0aec0c29db591c211a585e12e71
spaceC00kie <kirstie317@gmail.com>
2024-09-16 14:33:05 -0400

    no-plan init

---
+++ no-plan-backend/src/main/java/com/spacec00kie/noplanbackend/NoPlanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.noplanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoPlanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoPlanBackendApplication.class, args);
+    }
+
+}
---
+++ no-plan-backend/src/test/java/com/spacec00kie/noplanbackend/NoPlanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.noplanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoPlanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
