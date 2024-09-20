dc84cdf70c06f8745ab2e74b4074919672f8a2aa
spaceC00kie <kirstie317@gmail.com>
2024-09-20 16:24:17 -0400

    pokemon-search init

---
+++ pokemon-search-backend/src/main/java/com/spacec00kie/pokemonsearchbackend/PokemonSearchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.pokemonsearchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonSearchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonSearchBackendApplication.class, args);
+    }
+
+}
---
+++ pokemon-search-backend/src/test/java/com/spacec00kie/pokemonsearchbackend/PokemonSearchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.pokemonsearchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonSearchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
