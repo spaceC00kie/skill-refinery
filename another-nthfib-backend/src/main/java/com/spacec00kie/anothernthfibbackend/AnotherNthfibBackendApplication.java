a1908869b897e6bd9f9622d76a7fe7a3cb2e8ef7
spaceC00kie <kirstie317@gmail.com>
2024-09-24 14:05:47 -0400

    another-nthfib init

---
+++ another-nthfib-backend/src/main/java/com/spacec00kie/anothernthfibbackend/AnotherNthfibBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.anothernthfibbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherNthfibBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnotherNthfibBackendApplication.class, args);
+    }
+
+}
---
+++ another-nthfib-backend/src/test/java/com/spacec00kie/anothernthfibbackend/AnotherNthfibBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.anothernthfibbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherNthfibBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
