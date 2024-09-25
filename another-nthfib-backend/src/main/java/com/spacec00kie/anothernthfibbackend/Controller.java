f7ae1c72ded75ad3da0042e65a9e2512fda04172
spaceC00kie <kirstie317@gmail.com>
2024-09-24 23:07:27 -0400

    refined

--- another-nthfib-backend/src/main/java/com/spacec00kie/anothernthfibbackend/Controller.java
+++ another-nthfib-backend/src/main/java/com/spacec00kie/anothernthfibbackend/Controller.java
@@ -1,28 +1,33 @@
 package com.spacec00kie.anothernthfibbackend;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 public class Controller {
     @GetMapping("/nthfib/{n}")
-    public Number nthfib(@PathVariable Number n) {
-        // Convert the Number to int
-        int num = n.intValue();
+    public ResponseEntity<Map<String, Integer>> nthfib(@PathVariable int n) {
+        Map<String, Integer> map = new HashMap<>();
         // if n is less than 3
-        if (num < 3) {
+        if (n < 3) {
             // return n - 1
-            return num - 1;
+            map.put("fib", n - 1);
+            return ResponseEntity.ok(map);
         }
         // set count to 3
         int count = 2;
         // set previous array to [0, 1]
         int[] prev = {0, 1};
         // while count is less than n
-        while (count < num) {
+        while (count < n) {
             // set prev0 to prev1 and set prev1 to prev0+prev1
             int temp = prev[0];
             prev[0] = prev[1];
@@ -31,6 +36,7 @@ public class Controller {
             count++;
         }
         // return prev1
-        return prev[1];
+        map.put("fib", prev[1]);
+        return ResponseEntity.ok(map);
     }
 }
