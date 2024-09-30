e0ae75747750cf6f5cc5816f31e917c71475bbe2
spaceC00kie <kirstie317@gmail.com>
2024-09-29 20:49:09 -0400

    posting-again + backend init

---
+++ posting-again-backend/src/main/java/com/spacec00kie/postingagainbackend/PostingAgainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.postingagainbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostingAgainBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostingAgainBackendApplication.class, args);
+    }
+
+}
---
+++ posting-again-backend/src/test/java/com/spacec00kie/postingagainbackend/PostingAgainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.postingagainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingAgainBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
