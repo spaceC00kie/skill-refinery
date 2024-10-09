974d6006f6c9dae30a429cff0d9862cfb3e028c8
spaceC00kie <kirstie317@gmail.com>
2024-10-09 16:30:20 -0400

    easy-street init

---
+++ easy-street-backend/src/main/java/com/spacec00kie/easystreetbackend/EasyStreetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.easystreetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStreetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyStreetBackendApplication.class, args);
+    }
+
+}
---
+++ easy-street-backend/src/test/java/com/spacec00kie/easystreetbackend/EasyStreetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.easystreetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyStreetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
