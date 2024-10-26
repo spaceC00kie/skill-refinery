36ef2e70a03698647773a11f9ba86b64b8975ffb
spaceC00kie <kirstie317@gmail.com>
2024-10-26 18:20:16 -0400

    request-mapping init

---
+++ request-mapping-backend/request-mapping-backend/src/main/java/com/spaceC00kie/request_mapping_backend/RequestMappingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spaceC00kie.request_mapping_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestMappingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestMappingBackendApplication.class, args);
+	}
+
+}
---
+++ request-mapping-backend/request-mapping-backend/src/test/java/com/spaceC00kie/request_mapping_backend/RequestMappingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceC00kie.request_mapping_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestMappingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
