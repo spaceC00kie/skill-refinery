e9fea75b0ee300afce534ec45ea1a9f98e403c3c
spaceC00kie <kirstie317@gmail.com>
2024-10-27 20:37:02 -0400

    add get and post requests, input fields for new pokemon

--- request-mapping/src/App.tsx
+++ request-mapping/src/App.tsx
@@ -1,34 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  interface Pokemon {
+    id: number
+    name: string
+    color: string
+  }
+
+  const pokemonListUrl: string = "http://localhost:8080/pokemon/list"
+  const postingUrl: string = "http://localhost:8080/pokemon/add"
+  const [newPokeName, setNewPokeName] = useState("")
+  const [newPokeColor, setNewPokeColor] = useState("")
+  const [newPokeId, setNewPokeId] = useState<number>()
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>()
+  const newPokemon = {
+    id: newPokeId,
+    name: newPokeName,
+    color: newPokeColor,
+  }
+
+  function PostNewPokemon() {
+    fetch(postingUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newPokemon),
+    })
+      .then(res => res.json())
+      .then(setPokemonList)
+  }
+
+  useEffect(() => {
+    fetch(pokemonListUrl)
+      .then(res => res.json())
+      .then(setPokemonList)
+  }, [pokemonList])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col gap-2">
+      <div>Define a new Pokemon</div>
+      <div className="flex flex-col gap-1">
+        <div>
+          Id:
+          <input
+            type="number"
+            className="rounded-md"
+            onChange={e => setNewPokeId(e.target.valueAsNumber)}
+          />
+        </div>
+        <div>
+          Name:
+          <input
+            className="rounded-md"
+            onChange={e => setNewPokeName(e.target.value)}
+          />
+        </div>
+        <div>
+          Color:
+          <input
+            className="rounded-md"
+            onChange={e => setNewPokeColor(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <div>What you're making</div>
+          <div className="">
+            <div>Id: {newPokeId}</div>
+            <div>Name: {newPokeName}</div>
+            <div>Color: {newPokeColor}</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <button className="border border-gray-400" onClick={PostNewPokemon}>
+        Create pokemon
+      </button>
+      <div className="">
+        <div>List of all the Pokemon in our system:</div>
+        {pokemonList?.map((pokemon, index) => (
+          <div key={index} className="flex gap-2 border rounded-sm">
+            <div>Name: {pokemon.name}</div>
+            <div>Color: {pokemon.color}</div>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
