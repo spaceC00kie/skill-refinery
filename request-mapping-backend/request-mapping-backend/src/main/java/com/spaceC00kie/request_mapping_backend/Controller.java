e952583ab462b7f087e413066a128b444def7726
spaceC00kie <kirstie317@gmail.com>
2024-10-27 20:41:38 -0400

    create controller for get and post endpoints create Pokemon record

---
+++ request-mapping-backend/request-mapping-backend/src/main/java/com/spaceC00kie/request_mapping_backend/Controller.java
@@ -0,0 +1,23 @@
+package com.spaceC00kie.request_mapping_backend;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/pokemon")
+public class Controller {
+    List<Pokemon> pokemonList = new ArrayList<>(List.of(new Pokemon(1, "Bulbasaur", "green")));
+
+    @GetMapping("/list")
+    public List<Pokemon> list() {
+        return pokemonList;
+    }
+
+    @PostMapping("/add")
+    public List<Pokemon> add(@RequestBody Pokemon pokemon) {
+        pokemonList.add(pokemon);
+        return pokemonList;
+    }
+}
---
+++ request-mapping-backend/request-mapping-backend/src/main/java/com/spaceC00kie/request_mapping_backend/Pokemon.java
@@ -0,0 +1,4 @@
+package com.spaceC00kie.request_mapping_backend;
+
+public record Pokemon(int id, String name, String color) {
+}
