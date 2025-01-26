a4f54de74fb3ee2826482782e3c7d3e51693410f
spaceC00kie <kirstie317@gmail.com>
2025-01-26 15:35:44 -0500

    init

---
+++ hire-me-game-testing-backend/src/test/java/com/spacec00kie/hiremegametestingbackend/HireMeGameTestingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacec00kie.hiremegametestingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireMeGameTestingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
